0c0e5ce16d3022439c6667f214d447a0a4200497
mx.cogito <mx.cogito@2aad3f0e-3d4c-0410-9c25-07f19534b29f>
2008-05-03 14:50:27 +0000

    add config.h（included in sqliteInt.h）errorHandle.c errorHandle.h进行配置和错误处理. 用sqlite3Randomness初步生成测试数据,还不知道如何获得B-Tree中的page数。

---
+++ errorHandle.c
@@ -0,0 +1,12 @@
+#include "sqliteInt.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void errorHandle(int rc, const char* msg)
+{
+	if(rc!=SQLITE_OK){
+	  fprintf(stderr,"%s: %s\n",msg,"error message");
+	  exit(-1);
+	}
+}
+
---
+++ errorHandle.h
@@ -0,0 +1,5 @@
+#ifndef _ERROR_HANDLE_H_
+#define _ERROR_HANDLE_H_
+void errorHandle(int rc, const char* msg);
+#endif
+
