4df5e2e31b00d9da39df1e7f9ffb02d0edbd0381
mx.cogito <mx.cogito@2aad3f0e-3d4c-0410-9c25-07f19534b29f>
2008-05-03 09:53:47 +0000

    main函数测试成功，写了一个文件，正确性还待检验。

--- main.c
+++ main.c
@@ -1,10 +1,42 @@
 #include "sqliteInt.h"
-int main(){
-	Btree* btree;
-	// Open a B-Tree
-	int rc = sqlite3BtreeOpen("test.bt",&btree,1);
+#include <stdio.h>
+int main(){
+	const char* zFilename="test.bt";
+	Btree* pBt;
+	BtCursor* pCur;
+	int iTable;
+	int rc;
 	
-	
+	void* notUsed;
+	i64 nKey;
+	void * pData;
+	int nData;
+	
+	//= Create a B-Tree file =
+	//Open B-Tree
+	rc = sqlite3BtreeOpen(zFilename,&pBt,BTREE_OMIT_JOURNAL);
+	if(rc!=SQLITE_OK){	
+		fprintf(stderr,"can't open the data base file\n");
+	}
+
+	sqlite3BtreeBeginTrans(pBt, 1);
+	//Create a root page for the btree, the page no is iTable
+	rc = sqlite3BtreeCreateTable(pBt, &iTable, BTREE_INTKEY|BTREE_LEAFDATA);
+	if(rc!=SQLITE_OK)	fprintf(stderr,"can't create table\n");
+
+	//Create a Cursor for the B-Tree
+	sqlite3BtreeCursor(pBt, iTable, 1, NULL, notUsed, &pCur);
+	if(rc!=SQLITE_OK)	fprintf(stderr,"can't create btree cursor\n");
+
+	nKey = 777;
+	nData=11;
+	pData=sqlite3Malloc(nData);
+	
+	//= Insert data into the file =
+	rc = sqlite3BtreeInsert(pCur,notUsed,nKey,pData,nData);
+	if(rc!=SQLITE_OK)	fprintf(stderr,"can't create btree cursor\n");
+
+	sqlite3BtreeCommit( pBt);
 	printf("rc = %d\n",rc);
     
     // Do an insert and verify that the database file grows in size.
